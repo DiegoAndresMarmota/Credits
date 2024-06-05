5cf2e27852ad15b0ea507d2274092db6f6566e89
Diego Andres <diegoechalarp@hotmail.com>
2024-06-05 17:34:42 -0400

    Creación del main

--- data/cmd/main.go
+++ data/cmd/main.go
@@ -1,7 +1,21 @@
-package cmd
+package main
 
-import "fmt"
+import (
+	"credits/data/internal/controller/data"
+	"credits/data/internal/handler"
+	"credits/data/internal/repository/memory"
+	"log"
+	"net/http"
+)
 
-func data() {
-	fmt.Println("data")
+func main() {
+
+	log.Println("Starting the credits data service")
+	repo := memory.NewRepo()
+	cdt := data.NewController(repo)
+	h := handler.NewHandler(cdt)
+	http.Handle("/credits", http.HandlerFunc(h.GetData))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
