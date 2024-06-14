6f7de7bfb7d23623757cb4f3c68e8e73da713631
Diego Andres <diegoechalarp@hotmail.com>
2024-06-14 18:00:52 -0400

    Se agrega Main para Valuation

--- valuation/cmd/main.go
+++ valuation/cmd/main.go
@@ -1,7 +1,20 @@
 package cmd
 
-import "fmt"
+import (
+	"credits/valuation/internal/controller"
+	"credits/valuation/internal/handler"
+	"credits/valuation/internal/repository/memory"
+	"log"
+	"net/http"
+)
 
-func valuation() {
-	fmt.Println("valuation")
+func main() {
+	log.Println("Inicializando el servicio de evaluación")
+	repo := memory.NewRepoValuation()
+	ctrl := controller.NewValueService(repo)
+	h := handler.NewHandValuation(ctrl)
+	http.Handle("/valuation", http.HandlerFunc(h.Handle))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
