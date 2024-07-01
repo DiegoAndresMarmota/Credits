845ba93de311d9c2cf6dede3cb7bd4e5b5c57df5
Diego Andres <diegoechalarp@hotmail.com>
2024-07-01 18:15:00 -0400

    Modif.y creación de Register/Desregister de una dirección de una instancia.

--- balancer/pkg/memory/memory.go
+++ balancer/pkg/memory/memory.go
@@ -1,13 +1,14 @@
 package memory
 
 import (
+	"context"
 	"sync"
 	"time"
 )
 
-type service string
+type Service string
 
-type instanceID string
+type InstanceID string
 
 // ServiceInstance define una instancia de un servicio
 type ServiceInstance struct {
@@ -19,12 +20,40 @@ type ServiceInstance struct {
 // Registry define un registro de servicio en memoria
 type Registry struct {
 	sync.RWMutex
-	serviceAddrs map[service]map[instanceID]*ServiceInstance
+	serviceAddrs map[Service]map[InstanceID]*ServiceInstance
 }
 
 // NewRegistry crea un nuevo registro de servicio
 func NewRegistry() *Registry {
 	return &Registry{
-		serviceAddrs: make(map[service]map[instanceID]*ServiceInstance),
+		serviceAddrs: make(map[Service]map[InstanceID]*ServiceInstance),
 	}
+}
+
+// Register registra una instancia de un servicio
+func (re *Registry) Register(ctx context.Context, service string, instanceID string, host string) error {
+	re.Lock()
+	defer re.Unlock()
+
+	if _, ok := re.serviceAddrs[Service(service)]; !ok {
+		re.serviceAddrs[Service(service)] = make(map[InstanceID]*ServiceInstance)
+	}
+	re.serviceAddrs[Service(service)][InstanceID(instanceID)] = &ServiceInstance{
+		Host:     host,
+		Active:   true,
+		LastTime: time.Now(),
+	}
+	return nil
+}
+
+// Deregister desregistra una instancia de un servicio
+func (re *Registry) Deregister(ctx context.Context, service string, instanceID string) error {
+	re.Lock()
+	defer re.Unlock()
+
+	if _, ok := re.serviceAddrs[Service(service)]; ok {
+		delete(re.serviceAddrs[Service(service)], InstanceID(instanceID))
+		return nil
+	}
+	return nil
 }
