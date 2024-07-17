34ffc24578c42326133ee901ee1ccb5fc617d040
Diego Andres <diegoechalarp@hotmail.com>
2024-07-17 19:18:47 -0400

    Implementación hashicorp/consul

--- balancer/pkg/discovery/search/search.go
+++ balancer/pkg/discovery/search/search.go
@@ -14,19 +14,8 @@ type Registry struct {
 	client consul.Client
 }
 
-// NewRegistry crea una nueva instancia de un servicio de registro basado en Hashicorp/Consul.
-func NewRegistry(address string) (*Registry, error) {
-	config := consul.DefaultConfig()
-	config.Address = address
-	client, err := consul.NewClient(config)
-	if err != nil {
-		return nil, err
-	}
-	return &Registry{client: *client}, nil
-}
-
 // Register crea un servicio en el registro.
-func (re *Registry) Register(ctx context.Context, instanceID string, serviceName string, hostPort string) error {
+func (re *Registry) Instance(ctx context.Context, instanceID string, serviceName string, hostPort string) error {
 	split := strings.Split(hostPort, ":")
 	if len(split) != 2 {
 		return errors.New("hostPort must be in a form of host/port, example: localhost:8081")
@@ -48,9 +37,25 @@ func (re *Registry) Register(ctx context.Context, instanceID string, serviceName
 	})
 }
 
-// Deregister elimina un servicio del registro.
-func (re *Registry) Deregister(ctx context.Context, instanceID string, _ string) error {
+// NotInstance elimina un servicio del registro.
+func (re *Registry) NotInstance(ctx context.Context, instanceID string, serviceName string) error {
 	return re.client.Agent().ServiceDeregister(instanceID)
+}
+
+// ReportHeathyState verifica el estado de salud de reporte de cada servicio.
+func (re *Registry) ReportHealthyState(InstanceID string, _ string) error {
+	return re.client.Agent().PassTTL(InstanceID, "")
+}
+
+// NewRegistry crea una nueva instancia de un servicio de registro basado en Hashicorp/Consul.
+func NewRegistry(address string) (*Registry, error) {
+	config := consul.DefaultConfig()
+	config.Address = address
+	client, err := consul.NewClient(config)
+	if err != nil {
+		return nil, err
+	}
+	return &Registry{client: *client}, nil
 }
 
 // ServiceAddresses retorna una lista de direcciones de instancias activas para un servicio dado.
@@ -66,9 +71,4 @@ func (re *Registry) ServiceAddresses(ctx context.Context, serviceName string) ([
 		res = append(res, entry.Service.Address+":"+strconv.Itoa(entry.Service.Port))
 	}
 	return res, nil
-}
-
-// ReportHeathyState verifica el estado de salud de reporte de cada servicio.
-func (re *Registry) ReportHeathyState(InstanceID string, _ string) error {
-	return re.client.Agent().PassTTL(InstanceID, "")
 }
